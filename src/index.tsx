731a5b093d2cfd4678a963f6ce0ed28a42f76497
bhupendra <bhupendra214.rejoice@gmail.com>
2024-01-07 11:17:05 +0530

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import App from 'components/App'
 import './index.css'
-const container = document.getElementById('root') 
+const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
 root.render(<App />)
